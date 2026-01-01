055d54be31b72230b15158882a89a091c9e930bf
Shafin Kp <shafinkp151@gmail.com>
2026-01-01 16:58:18 +0000

    https://res.cloudinary.com/ddqzzqnjh/image/upload/v1767286657/hbn0rm8hof

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,21 +1,16 @@
-import type { SVGProps } from 'react';
+import Image from 'next/image';
+import type { HTMLAttributes } from 'react';
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo(props: HTMLAttributes<HTMLDivElement>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M20 12v10H4V12" />
-      <path d="M2 7h20v5H2z" />
-      <path d="M12 22V7" />
-      <path d="m15 7-3 5-3-5" />
-    </svg>
+    <div {...props}>
+      <Image
+        src="https://res.cloudinary.com/ddqzzqnjh/image/upload/v1767286657/hbn0rm8hof8mre3zu0dk.png"
+        alt="MRSHOPY Logo"
+        width={24}
+        height={24}
+        className="h-full w-full"
+      />
+    </div>
   );
 }
